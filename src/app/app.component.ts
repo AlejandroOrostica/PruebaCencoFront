7fac74b2878a79712c2de7426a6dcb21e352e0fe
Alejandro <alejandro.orostica@usach.cl>
2021-05-24 00:17:03 -0400

    Cenco task finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+  cities: any;
+  isLoaded: boolean;
+  constructor() {
+    this.cities = ['Santiago', 'Zurich', 'Auckland', 'Sydney', 'Londres', 'Georgia'],
+    this.isLoaded = false
+
+  }
+
+  ws = "ws://localhost:8000/ws"
+  subject = webSocket(this.ws);
+  messages: any = [];
+  weathers: any = [];
+
+  ngOnInit() {
+
+
+    this.consulting()
+
+  }
+
+  consulting(){
+
+    this.subject.subscribe(messages => {
+      this.isLoaded = true
+      this.messages = messages
+      console.log(messages)
+    } )
+     
+
+
+   }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
